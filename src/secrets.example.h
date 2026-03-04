ad35e8e2fbf4579e1d9e7d37be8cbbbd926d4695
Mehdi <46991431+Mehdi-Leqsiouer@users.noreply.github.com>
2026-03-04 11:05:14 +0100

    Fix syntax and update botToken in secrets.example.h

--- src/secrets.example.h
+++ src/secrets.example.h
@@ -1,6 +1,6 @@
 #pragma once
-const char* ssid      = "ssid"
+const char* ssid      = "ssid";
 const char* password  = "password";
-const String botToken = "bot_tojen";
+const String botToken = "bot_token";
 const String chatId   = "chat_id";
 const char* sheetsWebhookUrl   = "sheet_webhook_url";
